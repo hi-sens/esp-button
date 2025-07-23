6be0d48994984865caeb36bb120028ba7f1b2082
hi-sens <sens@desktop-jeffery.localdomain>
2025-07-23 16:29:42 +0800

    esp-button 的使用

--- components/Button/Button.cc
+++ components/Button/Button.cc
@@ -1,13 +1,10 @@
 #include "Button.h"
 #include "esp_log.h"
 #include <iostream>
-
+#include <cassert>
 
 Button::Button(uint32_t button_num, uint8_t active_level, bool enable_power_save){
-    button_config_t btn_cfg = {
-        .long_press_time = 2000,
-        .short_press_time = 0,
-    };
+    button_config_t btn_cfg = {0};
     button_gpio_config_t gpio_cfg = {
         .gpio_num = (int32_t)button_num,
         .active_level = active_level,
@@ -15,7 +12,7 @@ Button::Button(uint32_t button_num, uint8_t active_level, bool enable_power_save
         .disable_pull = false
     };
     esp_err_t ret = iot_button_new_gpio_device(&btn_cfg, &gpio_cfg, &btn_handle);
-    std::cout<< "Button created with handle: " << btn_handle << std::endl;
+    assert(ret == ESP_OK);
 }
 
 void Button::OnPressDown(std::function<void()> callback){
@@ -24,14 +21,15 @@ void Button::OnPressDown(std::function<void()> callback){
     }
     on_press_down_ = callback;
     if (btn_handle != nullptr) {
-        printf("Registering button callback\n");
         iot_button_register_cb(btn_handle, BUTTON_PRESS_DOWN, NULL, [](void *button_handle, void *usr_data){
-            printf("Button pressed down callback triggered\n");
             Button *button = static_cast<Button*>(usr_data);
             if (button->on_press_down_) {
                 button->on_press_down_();
             }
         }, this);
+
+
+        
     }
 }
 Button::~Button()
--- main/esp-button.cc
+++ main/esp-button.cc
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "Button.h"
+#include "freertos/FreeRTOS.h"
 
 extern "C" void app_main(void)
 {
-    printf("Hello world!\n");
-    Button button(7, 0, false);
+    Button button(10, 0, false);
     button.OnPressDown([](){
         printf("Button pressed down\n");
     });
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(1000));  // Keep the task alive
+    }
 }
+
